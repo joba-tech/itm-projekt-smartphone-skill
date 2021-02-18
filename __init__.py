b82958f9b9d118e5486dcf3361b8e1677788761f
joba-tech <service.joba@gmail.com>
2021-02-18 19:59:55 +0100

    choice

--- __init__.py
+++ __init__.py
@@ -6,8 +6,9 @@ class ItmProjektSmartphone(MycroftSkill):
         MycroftSkill.__init__(self)
 
     @intent_handler('first.step.intent')
-    def handle_test(self, message):
-        self.speak_dialog('welcome')
+    def handle_first_step_intent(self):
+        phone_choice = self.get_response('which.phone.do.you.want')
+        self.speak_dialog('confirm.phone.choice', {'phone': phone_choice})
 
 
 def create_skill():

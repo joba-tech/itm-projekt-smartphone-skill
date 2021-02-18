3a38b75286898ae613f8c726f0033b704718af76
joba-tech <service.joba@gmail.com>
2021-02-18 19:04:31 +0100

    bug

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ class ItmProjektSmartphone(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('ItmProjektSmartphone.intent')
+    @intent_file_handler('first.step.intent')
     def handle_test(self, message):
         self.speak_dialog('welcome')
 

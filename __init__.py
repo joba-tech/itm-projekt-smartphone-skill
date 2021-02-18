ef49bc662c523d3d2356122b12f6a711f00878db
joba-tech <service.joba@gmail.com>
2021-02-18 19:54:18 +0100

    intent_handler

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 
 class ItmProjektSmartphone(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('first.step.intent')
+    @intent_handler('first.step.intent')
     def handle_test(self, message):
         self.speak_dialog('welcome')
 

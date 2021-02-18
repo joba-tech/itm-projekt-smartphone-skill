ac4a418fed92e82fc4015ebcff3e62a13257d5e9
joba-tech <service.joba@gmail.com>
2021-02-18 20:44:15 +0100

    switch case

--- __init__.py
+++ __init__.py
@@ -5,6 +5,9 @@ class ItmProjektSmartphone(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
         self.phones = ['iPhone', 'Samsung', 'Nokia']
+        self.colors.iphone = ['Black', 'White']
+        self.colors.Samsung = ['Blue', 'White']
+        self.colors.Nokia = ['Yellow', 'Blue']
 
     @intent_handler('want.to.buy.a.phone.intent')
     def handle_want_to_buy_a_phone_intent(self):
@@ -12,6 +15,23 @@ class ItmProjektSmartphone(MycroftSkill):
         selection = self.ask_selection(self.phones, 'which.phone.do.you.want')
         self.speak_dialog('confirm.phone.choice', {'phone': selection})
 
+        def iphone():
+            return "Choose a color for your iPhone"
+
+        def samsung():
+            return "Choose a color for your Samsung"
+
+        def nokia():
+            return "Chosse a color for your Nokia"
+
+        def numbers_to_choice(numbers):
+            switcher = {
+                1: iphone,
+                2: samsung,
+                3: nokia
+            }
+            func = switcher.get(argument, lambda: "Error")
+            print func()
 
 def create_skill():
     return ItmProjektSmartphone()

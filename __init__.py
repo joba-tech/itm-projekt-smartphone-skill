93b5353522900e4b543bebaf2cf9426598969672
joba-tech <service.joba@gmail.com>
2021-02-18 18:13:22 +0100

    bugfixes

--- __init__.py
+++ __init__.py
@@ -1,13 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 
 class ItmProjektSmartphone(MycroftSkill):
-    def __init__(self):
-        MycroftSkill.__init__(self)
 
-    @intent_file_handler('initial.intent')
+    @intent_handler('initial.intent')
     def handle_set_phone_wish(self):
-        phone_wish = self.get_response('initial.dialog')
+        phone_wish = self.get_response('which.phone.do.you.want')
         self.speak_dialog('confirm.phone.wish'. {'phone': phone_wish })
 
 

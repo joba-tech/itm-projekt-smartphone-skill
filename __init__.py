5f99b43687464204cbb90118d696570dc002c8cc
joba-tech <service.joba@gmail.com>
2021-02-18 20:16:08 +0100

    list

--- __init__.py
+++ __init__.py
@@ -4,11 +4,13 @@ from mycroft import MycroftSkill, intent_handler
 class ItmProjektSmartphone(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.phones = ['iPhone', 'Samsung', 'Nokia']
 
-    @intent_handler('first.step.intent')
-    def handle_first_step_intent(self):
-        phone_choice = self.get_response('which.phone.do.you.want')
-        self.speak_dialog('confirm.phone.choice', {'phone': phone_choice} phone)
+    @intent_handler('want.to.buy.a.phone.intent')
+    def handle_want_to_buy_a_phone_intent(self):
+        self.speak_dialog('welcome')
+        selection = self.ask_selection(self.phones, 'which.phone.do.you.want')
+        self.speak_dialog('confirm.phone.choice', {'phone': phone_choice})
 
 
 def create_skill():

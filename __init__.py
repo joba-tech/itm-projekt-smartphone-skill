647434309dfacd206dabfa7dd918f082ed2a4498
joba-tech <service.joba@gmail.com>
2021-02-18 21:15:44 +0100

    selection

--- __init__.py
+++ __init__.py
@@ -8,19 +8,19 @@ class ItmProjektSmartphone(MycroftSkill):
         self.colorsIphone = ['Black', 'White']
         self.colorsSamsung = ['Blue', 'White']
         self.colorsNokia = ['Yellow', 'Blue']
-        self.smartphoneSelection
+        self.selection
 
     @intent_handler('want.to.buy.a.phone.intent')
     def handle_want_to_buy_a_phone_intent(self):
         self.speak_dialog('welcome')
-        smartphoneSelection = self.ask_selection(self.phones, 'which.phone.do.you.want')
+        selection = self.ask_selection(self.phones, 'which.phone.do.you.want')
         self.speak_dialog('confirm.phone.choice', {'phone': selection})
 
-        if smartphoneSelection == "iphone":
+        if selection == "iphone":
             self.speak_dialog('iphone.costs')
-        elif smartphoneSelection == "samsung":
+        elif selection == "samsung":
             self.speak_dialog('samsung.costs')
-        elif smartphoneSelection == "nokia":
+        elif selection == "nokia":
             self.speak_dialog('nokia.costs')
         else:
             self.speak_dialog('error')

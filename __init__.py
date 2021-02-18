94bc4b34c5aa0bff7f41437836341631171caf63
joba-tech <service.joba@gmail.com>
2021-02-18 21:16:37 +0100

    bug

--- __init__.py
+++ __init__.py
@@ -8,12 +8,11 @@ class ItmProjektSmartphone(MycroftSkill):
         self.colorsIphone = ['Black', 'White']
         self.colorsSamsung = ['Blue', 'White']
         self.colorsNokia = ['Yellow', 'Blue']
-        self.selection
 
     @intent_handler('want.to.buy.a.phone.intent')
     def handle_want_to_buy_a_phone_intent(self):
         self.speak_dialog('welcome')
-        selection = self.ask_selection(self.phones, 'which.phone.do.you.want')
+        self.selection = self.ask_selection(self.phones, 'which.phone.do.you.want')
         self.speak_dialog('confirm.phone.choice', {'phone': selection})
 
         if selection == "iphone":

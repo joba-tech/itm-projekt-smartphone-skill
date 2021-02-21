434ec94a5f7776d35636ea517ba79d9741cb3a9c
joba-tech <service.joba@gmail.com>
2021-02-21 17:42:13 +0100

    added intent

--- __init__.py
+++ __init__.py
@@ -96,7 +96,7 @@ class ItmProjektSmartphone(MycroftSkill):
         self.speak_dialog('welcome')
         self.speak_dialog('shopping.cart.status', {'phone1_name': self.cart[0][0], 'phone1_number': self.cart[0][1], 'phone2_name': self.cart[1][0], 'phone2_number': self.cart[1][1], 'phone3_name': self.cart[2][0], 'phone3_number': self.cart[2][1]})
 
-    @intent_handler('delete.shopping.cart')
+    @intent_handler('delete.shopping.cart.intent')
     def delete_shopping_cart_intent(self):
         self.speak_dialog('welcome')
         delete_shopping_cart = self.ask_yesno('are.you.sure.cart')
